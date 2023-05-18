21378770b11995b998c4a07171b9b6657939595e
gexterio <gexterio@gmail.com>
2023-05-18 18:30:49 +0400

    init commit

---
+++ src/main/java/ua/com/foxminded/universityassigmentservice/UniversityAssigmentServiceApplication.java
@@ -0,0 +1,13 @@
+package ua.com.foxminded.universityassigmentservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversityAssigmentServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UniversityAssigmentServiceApplication.class, args);
+    }
+
+}
